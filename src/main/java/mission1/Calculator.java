b4661267f18cd064f62d2b822b2e4c8c60cc5a89
sghwan <anan123456@naver.com>
2022-10-13 17:00:10 +0900

    disunite operator judge method

--- src/main/java/mission1/Calculator.java
+++ src/main/java/mission1/Calculator.java
@@ -1,38 +1,33 @@
 package mission1;
 
 public class Calculator {
+    public boolean isOperator(String value) {
+        if (value.equals("+") || value.equals("-") || value.equals("*") || value.equals("/"))
+            return true;
+
+        return false;
+    }
+
     public int calculate(String input) {
         String[] values = input.split(" ");
         int result = 0;
         String operator = "";
         for (int i = 0; i < values.length; i++) {
-            switch (values[i]) {
-                case "+":
-                    operator = "+";
-                    break;
-                case "-":
-                    operator = "-";
-                    break;
-                case "*":
-                    operator = "*";
-                    break;
-                case "/":
-                    operator = "/";
-                    break;
-                default:
-                    if (operator.equals("")) {
-                        result = Integer.parseInt(values[i]);
-                    } else {
-                        if (operator.equals("+"))
-                            result += Integer.parseInt(values[i]);
-                        if (operator.equals("-"))
-                            result -= Integer.parseInt(values[i]);
-                        if (operator.equals("*"))
-                            result *= Integer.parseInt(values[i]);
-                        if (operator.equals("/"))
-                            result /= Integer.parseInt(values[i]);
-                    }
-                    break;
+            if (isOperator(values[i]))
+                operator = values[i];
+            else {
+                if (operator.equals("")) {
+                    result = Integer.parseInt(values[i]);
+                } else {
+                    if (operator.equals("+"))
+                        result += Integer.parseInt(values[i]);
+                    if (operator.equals("-"))
+                        result -= Integer.parseInt(values[i]);
+                    if (operator.equals("*"))
+                        result *= Integer.parseInt(values[i]);
+                    if (operator.equals("/"))
+                        result /= Integer.parseInt(values[i]);
+                }
             }
         }
         return result;
