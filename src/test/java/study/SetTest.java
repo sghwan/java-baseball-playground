dc2eacea84eb172bda04d96ffac9ceeb7913dec2
sghwan <anan123456@naver.com>
2022-10-13 14:04:33 +0900

    Set size() 메method test

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,31 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+    Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 크기를 size() 메서드를 통해 확인")
+    void size() {
+        //Set Collection은 중복을 허락하지 않는다.
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
