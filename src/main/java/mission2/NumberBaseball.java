2973dbb0c3378e90ab9091dfff20261e36d560bf
sghwan <anan123456@naver.com>
2022-10-14 20:02:10 +0900

    feat: userInput validatoin

--- src/main/java/mission2/NumberBaseball.java
+++ src/main/java/mission2/NumberBaseball.java
@@ -23,6 +23,40 @@ public class NumberBaseball {
         return sb.toString();
     }
 
+    public boolean checkStringLength(String value) {
+        if (value.length() > 3)
+            return false;
+
+        return true;
+    }
+
+    public boolean compareNumber(int start, int length, String value) {
+        for (int i = start + 1; i < length; i++) {
+            if (value.charAt(start) == value.charAt(i))
+                return true;
+        }
+
+        return false;
+    }
+
+    public boolean checkDuplicateNumber(String value) {
+        int length = value.length();
+
+        for (int i = 0; i < length - 1; i++) {
+            if (compareNumber(i, length, value))
+                return true;
+        }
+
+        return false;
+    }
+
+    public boolean validateUserInput(String userInput) {
+        if (!checkStringLength(userInput) || checkDuplicateNumber(userInput))
+            return false;
+
+        return true;
+    }
+
     public String getGameNumber() {
         return gameNumber;
     }
--- src/test/java/mission2/NumberBaseballTest.java
+++ src/test/java/mission2/NumberBaseballTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.ArrayList;
 
@@ -19,13 +21,21 @@ public class NumberBaseballTest {
     }
 
     @Test
-    @DisplayName("인스턴스가 생성될 시 생성자에서 pickNumbers()가 호출되어 게임 숫자가 세팅된다. ")
+    @DisplayName("인스턴스가 생성될 시 생성자에서 pickNumbers()가 호출되어 게임 숫자가 세팅된다.")
     void pickNumbers() {
         ArrayList<Integer> list = numberBaseball.getNumberList();
         String gameNumber = numberBaseball.getGameNumber();
 
         assertThat(list.size()).isEqualTo(6);
         assertThat(gameNumber.length()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"123,true", "456,true", "121,false", "1234,false"})
+    @DisplayName("사용자가 입력한 값이 3자리이고 중복이 없는 수인지 확인한다.")
+    void validateUserInput(String userInput, boolean expected) {
+        boolean actual = numberBaseball.validateUserInput(userInput);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @AfterEach
